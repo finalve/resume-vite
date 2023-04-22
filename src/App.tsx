623d6aca5588ddd6052138333e68b25debc0fab0
waleesin <veewutsing@gmail.com>
2023-04-23 05:25:00 +0700

    c

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,11 @@ import {
   Route,
   RouterProvider,
 } from "react-router-dom";
-import Root from './components/Root';
 import Dashboard from './components/Dashboard';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path="/" element={<Root />}>
+    <Route path="/" element={<Dashboard />}>
         <Route path="" element={<Dashboard />} />
       <Route path="dashboard" element={<Dashboard />} />
       {/* ... etc. */}
